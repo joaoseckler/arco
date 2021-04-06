76d06c1338be378ae1c3f8a49b1b913e73ed41c2
João Seckler <jseckler@riseup.net>
2021-04-06 16:19:19 -0300

    Update holerites

--- holerites/generate.sh
+++ holerites/generate.sh
@@ -2,6 +2,14 @@
 
 cd files
 
+
+for i in *; do
+  if [ "$i" != "in.pdf" ]; then
+    rm -f "$i"
+  fi
+done
+rm -f TUDO.zip
+
 pdftk "in.pdf" burst
 
 for i in pg_*.pdf; do
@@ -11,7 +19,7 @@ done
 for i in pg_*.txt; do
   name=$(cat "$i" | head -10 | tail -1 | cut -d' ' -f 2-)
   date=$(cat "$i" | head -8 | tail -1 | cut -d' ' -f 2 | sed 's$/$_$g')
-  mv "${i%.txt}.pdf" "${date}_HOLERITE_$name.pdf"
+  mv "${i%.txt}.pdf" "${date}_HOLERITE_${name// /_}.pdf"
 done
 
 rm *.txt
